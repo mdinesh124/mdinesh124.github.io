7c4e44c8d78bf228385835a79421395526654f07
mdinesh124 <79132253+mdinesh124@users.noreply.github.com>
2021-07-19 23:19:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+$(document).ready(function() 
+{
+
+    $(window).scroll(function() 
+	{
+        if (this.scrollY > 20) 
+		{
+            		$('.navbar').addClass("sticky");
+        	} 
+	else 
+		{
+            		$('.navbar').removeClass("sticky");
+        	}
+    	});
+
+    $('.menu-btn').click(function() 
+		{
+        		$('.navbar .menu').toggleClass("active");
+        		$('.menu-btn i').toggleClass("active");
+    		});
+
+
+var typed = new Typed(".type", {
+        strings: ["Student", "Code enthusiast", "Web Developer", "Melomaniac"],
+        typeSpeed: 70,
+        backSpeed: 75,
+        loop: true
+    });
+
+
+});
+
+
+
+
+
+
+
+
+
+
+function openNav() {
+	document.getElementById("mySidebar").style.width = "50%";
+	document.getElementById("main").style.marginRight = "50%";
+  }
+  
+  function closeNav() {
+	document.getElementById("mySidebar").style.width = "0";
+	document.getElementById("main").style.marginRight= "0";
+  }
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
